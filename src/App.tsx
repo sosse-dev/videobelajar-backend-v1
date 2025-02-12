2eb94d6def3cebd6bc798ac16d77b191c848ca7a
sosse-dev <sosse3135@gmail.com>
2025-02-12 20:32:44 +0800

    fix: route userslist

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ const App: React.FC = () => {
           }
         />
         <Route
-          path="/users-list"
+          path="/userslist"
           element={
             <ProtectedRoute>
               <Users />
